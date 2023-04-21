624a0a27bd0aa87b35dd42d553ba3a17c4cbd724
P0m1dorka <loginovandrej31@gmail.com>
2023-04-21 18:54:50 +0300

    done pocemon collection

--- PokemonGame/Game/Pokemon.cs
+++ PokemonGame/Game/Pokemon.cs
@@ -2,6 +2,20 @@ namespace PokemonGame;
 
 public class Pokemon : ICloneable
 {
+    private float attack { get; set; }
+    private float defence { get; set; }
+    private float hp { get; set; }
+    private float[] againstTypeTable { get; set; }
+    private int base_total { get; set; }
+    private int capture_rate { get; set; }
+    private int sp_attacl { get; set; }
+    private int sp_defence { get; set; }
+    private int speed { get; set; }
+    private int generation { get; set; }
+    private bool is_legend { get; set; }
+    public string name { get; set; }
+    private PokemonType type1 { get; set; }
+    private PokemonType? type2 { get; set; }
     public Pokemon(
         float attack,
         int base_total,
@@ -23,19 +37,35 @@ public class Pokemon : ICloneable
 
     public object Clone()
     {
-        throw new NotImplementedException();
+
+        Pokemon pok = new Pokemon(attack, base_total, capture_rate, defence, hp, sp_attacl, sp_defence, speed, generation, is_legend, name, type1, type2, againstTypeTable);
+        return pok;
     }
 
     public bool isDead
     {
         get
         {
-            throw new NotImplementedException();
+            if(this.hp < 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            };
         }
     }
 
     public void AttackPokemon(Pokemon target)
     {
+        if (target.type2 != null)
+        {
+            
+        }
+
+
+
         throw new NotImplementedException();
     }
 }
--- PokemonGame/Game/PokemonCollection.cs
+++ PokemonGame/Game/PokemonCollection.cs
@@ -1,25 +1,82 @@
+using System.Drawing;
 using System.Globalization;
 namespace PokemonGame;
 
 public class PokemonCollection
 {
+    const int SIZE = 1000;
+    Node[] container = new Node[SIZE];
+    class Node
+    {
+        public Pokemon pokemon;
+        public Node? next;
+        public Node(Pokemon pokemon, Node? next)
+        {
+            this.pokemon = pokemon;
+            this.next = next;
+        }
+
+    }
     public PokemonCollection(string dataPath)
     {
-        throw new NotImplementedException();
+        string[] data = File.ReadAllLines(dataPath);
     }
 
     public void ParseData(string[] data)
     {
-        throw new NotImplementedException();
+        foreach (string link in data)
+        {
+            string[] mass = link.Split(";");
+            float[] againttypetable = new float[18];
+            for(int i = 0; i < againttypetable.Length; i++)
+            {
+                againttypetable[i] = float.Parse(mass[i]);
+            }
+            int[] intmass = new int[8];
+            intmass[0] = Convert.ToInt32(mass[18]); //attack
+            intmass[1] = Convert.ToInt32(mass[19]); //base_total
+            intmass[2] = Convert.ToInt32(mass[20]); //capture
+            intmass[3] = Convert.ToInt32(mass[21]); //defense
+            intmass[4] = Convert.ToInt32(mass[22]); //hp
+            string name = mass[23];
+            intmass[5] = Convert.ToInt32(mass[24]); //sp_attack
+            intmass[6] = Convert.ToInt32(mass[25]); //sp_defence
+            intmass[7] = Convert.ToInt32(mass[26]); //speed
+            PokemonType type1 = (PokemonType)Enum.Parse(typeof(PokemonType), mass[27]);
+             PokemonType?   type2 = mass[28] == "" ? null : (PokemonType)Enum.Parse(typeof(PokemonType), mass[28]);
+            int generat = Convert.ToInt32(mass[29]);
+            bool legend = Convert.ToBoolean(mass[30]);
+            Pokemon poc = new Pokemon(intmass[0], intmass[1], intmass[2], intmass[3], intmass[4], intmass[5], intmass[6], intmass[7], generat, legend, name, type1, type2,againttypetable);
+            Push(poc);
+        }
     }
 
     public void Push(Pokemon pokemon)
     {
-        throw new NotImplementedException();
+        int hasg = Math.Abs(pokemon.name.GetHashCode()%container.Length);
+        if (container[hasg] == null)
+        {
+            container[hasg] = new Node(pokemon,null);
+        }
+        else
+        {
+            Node buf = container[hasg];
+            container[hasg] = new Node(pokemon,buf);
+        }
     }
 
     public Pokemon FindByName(string name)
     {
-        throw new NotImplementedException();
+        int hash = Math.Abs(name.GetHashCode()) % container.Length;
+        Node buf = container[hash];
+        while (buf.pokemon.name != name)
+        {
+            buf = buf.next;
+        }
+        if (buf is null)
+        {
+            throw new NotImplementedException();
+        }
+        return buf.pokemon;
     }
 }
