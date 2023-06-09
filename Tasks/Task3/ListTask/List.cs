1187bf86e0fb43cdcc710373b08735faed3f8d70
sl3d <81529806+P0m1dorka@users.noreply.github.com>
2023-06-09 18:47:10 +0300

    done 7 tests

--- Tasks/Task3/ListTask/List.cs
+++ Tasks/Task3/ListTask/List.cs
@@ -2,6 +2,13 @@ using System.Collections.Generic;
 using System;
 namespace ListTask 
 {
+    class Program{
+
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("df");
+        }
+    }
     public class List
     {
         class Node
@@ -15,63 +22,125 @@ namespace ListTask
         private Node tail;
         private int size;
 
-        public List() {
-            throw new NotImplementedException();
+        public List() 
+        {
+            head = null;
+            tail = null;
+            size = 0;   
         }
 
         public List(IEnumerable<int> collection) 
         {
-            throw new NotImplementedException();
+            foreach (int size in collection)
+            {
+                PushBack(size);
+            }  
         }
 
         public int Front()
         {
-            throw new NotImplementedException();
+            return this.head.value;
         }
 
         public int Back()
         {
-            throw new NotImplementedException();
+            return this.tail.value;
         }
 
         public bool Empty()
         {
-            throw new NotImplementedException();
+            if(this.size == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public int Size()
         {
-            throw new NotImplementedException();
+            return this.size;
         }
-
         public void Clear()
         {
-            throw new NotImplementedException();
+            head = null;
+            tail = null;
+            size = 0;
         }
-
         public void PushBack(int value)
         {
-            throw new NotImplementedException();
+           Node newNode = new Node(); //создали новый нод
+           newNode.value = value; //присвоили ему значение от пользователя
+           if(tail is Node && head is Node){ //
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+           } 
+           else{
+            head = newNode;
+            tail = newNode;
+           }
+           size++;
         }
-
         public int PopBack()
         {
-            throw new NotImplementedException();
+            int buf = tail.value;
+            tail = tail.prev;
+            tail.next = null;
+            size--;
+            return buf;
         }
-
         public void PushFront(int value)
         {
-            throw new NotImplementedException();
+             Node newNode = new Node();
+            newNode.value = value;
+
+            if (tail is Node && head is Node)
+            {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            else
+            {
+                head = newNode;
+                tail = newNode;
+            }
+
+            size++;
         }
 
         public int PopFront()
         {
-            throw new NotImplementedException();
+            size--;
+            int buf = head.value;
+            head = head.next;
+            return buf; 
         }
 
         public void Resize(int count)
         {
-            throw new NotImplementedException();
+           if (count > size)
+            {
+                int a = count - size;
+                for (int i = 0; i < a; i++)
+                {
+                    PushBack(0);
+                }
+            }
+            else if (count < size)
+            {
+                int b = size - count;
+                for (int i = 0; i < b; i++) //��������
+
+                { 
+                        PopBack();
+                }
+
+            }
+            size = count;
         }
 
         public void Swap(List other_list)
